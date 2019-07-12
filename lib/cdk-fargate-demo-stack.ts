3145373d65dd2839f6aea712d2e337dbbdc744ad
Eric Odell <eric.odell@ucop.edu>
2019-07-12 05:25:24 +0000

    add vpc, ecs clusted and hello fargate service

--- lib/cdk-fargate-demo-stack.ts
+++ lib/cdk-fargate-demo-stack.ts
@@ -1,9 +1,30 @@
 import cdk = require('@aws-cdk/core');
+// import apigateway = require('@aws-cdk/aws-apigateway');
+import ec2 = require('@aws-cdk/aws-ec2');
+import ecs = require('@aws-cdk/aws-ecs');
+import ecs_patterns = require('@aws-cdk/aws-ecs-patterns');
+import lambda = require('@aws-cdk/aws-lambda');
+
 
 export class CdkFargateDemoStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create VPC
+    // NOTE: Limit AZs to avoid reaching resource quotas
+    const vpc = new ec2.Vpc(this, 'VPC', { maxAzs: 2 });
+
+    // Create ECS Cluster
+    const cluster = new ecs.Cluster(this, 'Cluster', { vpc });
+
+    // Instantiate Fargate Service with just cluster and image
+    const fargateService = new ecs_patterns.LoadBalancedFargateService(this, "FargateService", {
+      cluster,
+      image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
+    });
+
+    // Output the DNS where you can access your service
+    new cdk.CfnOutput(this, 'LoadBalancerDNS', { value: fargateService.loadBalancer.loadBalancerDnsName });
   }
+
 }
