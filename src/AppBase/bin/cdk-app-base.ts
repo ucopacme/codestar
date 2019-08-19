ffce7c7dc38b655f8f6d099f7388668c36ef73d5
Eric Odell <eric.odell@ucop.edu>
2019-08-19 22:58:47 +0000

    initial commit for app base template

--- src/AppBase/bin/cdk-app-base.ts
+++ src/AppBase/bin/cdk-app-base.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import cdk = require('@aws-cdk/core');
-import { CdkFargateDemoStack } from '../lib/cdk-app-base-stack';
+import { BaseAppStack } from '../lib/cdk-app-base-stack';
 
 const app = new cdk.App();
-new CdkFargateDemoStack(app, 'AppBase');
+new BaseAppStack(app, 'AppBase');
